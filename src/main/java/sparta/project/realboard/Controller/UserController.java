db2da6dcde0b1735208d3157233bc9daf82004b4
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2022-10-13 17:02:11 +0900

    사용안하는 일부 라이브러리 제거

--- src/main/java/sparta/project/realboard/Controller/UserController.java
+++ src/main/java/sparta/project/realboard/Controller/UserController.java
@@ -2,21 +2,14 @@ package sparta.project.realboard.Controller;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import sparta.project.realboard.Dto.LoginRequestDto;
 import sparta.project.realboard.Dto.UserRequestDto;
 import sparta.project.realboard.Entity.User;
-import sparta.project.realboard.Jwt.JwtProperties;
 import sparta.project.realboard.Repository.UserRepository;
 import sparta.project.realboard.Response.SingleResponse;
-import sparta.project.realboard.Security.UserDetailsImpl;
 import sparta.project.realboard.Service.ResponseService;
 import sparta.project.realboard.Service.UserService;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
 @CrossOrigin
